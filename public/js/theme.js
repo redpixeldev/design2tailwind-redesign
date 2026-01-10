89944e651711f7a752fa3468302fb7547dbf02d0
Vivian Guillen <me@vivian.do>
2026-01-10 14:40:26 -0400

    Align marketing copy and UI IDs across components

--- public/js/theme.js
+++ public/js/theme.js
@@ -468,8 +468,8 @@
 $(window).on ('load', function (){ // makes sure the whole site is loaded
 
 // -------------------- Site Preloader
-        $('#ctn-preloader').fadeOut(); // will first fade out the loading animation
-        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        // $('#ctn-preloader').fadeOut(); // will first fade out the loading animation
+        $('#preloader').fadeOut(); // will fade out the white DIV that covers the website.
         // $('body').delay(350).css({'overflow':'visible'});
 
 
