314ee4c33222417403d08a1937f7672bfdc75faa
Vivian Guillen <me@vivian.do>
2026-01-10 16:31:31 -0400

    Configure Cloudflare adapter and build optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,30 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
-	compressHTML: false,
-	build: {
-		assets: 'assets',
-		format: 'file',
-	},
-	vite: {
-		define: {
-			'process.env': process.env,
-		},
-		build: {
-			assetsInlineLimit: 0,
-			rollupOptions: {
-				output: {
-					entryFileNames: 'assets/main.js',
-					assetFileNames: 'assets/main[extname]',
-				},
-			},
-		},
+  compressHTML: false,
+
+  build: {
+      assets: 'assets',
+      format: 'file',
 	},
 
-	output: 'static',
+  vite: {
+      define: {
+          'process.env': process.env,
+      },
+      build: {
+          assetsInlineLimit: 0,
+          rollupOptions: {
+              output: {
+                  entryFileNames: 'assets/main.js',
+                  assetFileNames: 'assets/main[extname]',
+              },
+          },
+      },
+	},
+
+  output: 'static',
+  adapter: cloudflare(),
 });
