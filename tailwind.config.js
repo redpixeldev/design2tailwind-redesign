e4700e86e6129b057dc21abfe6017ab89b9d4674
Vivian Guillen <me@vivian.do>
2026-01-14 18:31:03 -0400

    Standardize responsive classes and simplify breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,68 +19,40 @@ module.exports = {
     },
     extend: {
       screens: {
-        // Legacy Bootstrap-style breakpoints (to be removed gradually)
-        '2xl': {
-          'min': '1200px',
-          'max': '1399px'
-        },
-        'xxl': {
-          'min': '1536px',
-        },
-        'xl': {
-          'min': '1200px',
-        },
-        'lg': {
-          'min': '992px',
-          'max': '1199.98px'
-        },
-        'md': {
-          'min': '768px',
-          'max': '991.98px'
-        },
-        'sm': {
-          'min': '576px',
-          'max': '767.98px'
-        },
-        'xsm': {
-          'max': '575.98px'
-        },
-        // Custom mobile-first breakpoint for small mobile
+        // Custom breakpoint for small mobile (480px+)
         'xs': '480px',
+        // Tailwind defaults are used: sm 640px, md 768px, lg 1024px, xl 1280px, 2xl 1536px
       },
     },
   },
   plugins: [
-    function ({
-    addComponents
-  })
-
-	 {
-    addComponents({
-      '.container': {
-        maxWidth: '100%',
-				margin: 'auto',
-        '@screen sm': {
-          maxWidth: '540px',
-					margin: 'auto',
-        },
-        '@screen md': {
-          maxWidth: '720px',
-					margin: 'auto',
-        },
-        '@screen lg': {
-          maxWidth: '960px',
-					margin: 'auto',
-        },
-        '@screen xl': {
-          maxWidth: '1140px',
-					margin: 'auto',
-        },
-        '@screen xxl': {
-          maxWidth: '1320px',
-					margin: 'auto',
-        },
-      }
-    })
-  }],
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          margin: 'auto',
+          '@screen sm': {
+            maxWidth: '640px',
+            margin: 'auto',
+          },
+          '@screen md': {
+            maxWidth: '720px',
+            margin: 'auto',
+          },
+          '@screen lg': {
+            maxWidth: '960px',
+            margin: 'auto',
+          },
+          '@screen xl': {
+            maxWidth: '1140px',
+            margin: 'auto',
+          },
+          '@screen 2xl': {
+            maxWidth: '1320px',
+            margin: 'auto',
+          },
+        }
+      })
+    }
+  ],
 }
