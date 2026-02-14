cd8d2bb05bc46577d517e64099f45547c2395948
Vivian Guillen <me@vivian.do>
2026-02-14 12:37:12 -0400

    Add RSS feed support and link in footer

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,27 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import type { APIContext } from 'astro';
+import { siteConfig } from '@config/site';
+
+export async function GET(context: APIContext) {
+	const posts = (
+		await getCollection('blog', ({ data }) => !data.draft)
+	).sort(
+		(a, b) =>
+			b.data.publishing_date.getTime() - a.data.publishing_date.getTime()
+	);
+
+	return rss({
+		title: `${siteConfig.name} Blog`,
+		description: siteConfig.description,
+		site: context.site!,
+		items: posts.map((post) => ({
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.publishing_date,
+			link: post.data.permalink,
+			author: post.data.author,
+		})),
+		customData: '<language>en-us</language>',
+	});
+}
